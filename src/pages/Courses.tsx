a22af2973a2b7d431a8d04cd624dd5190cf95ba6
nafiswatsiq <amrullohnafis@gmail.com>
2025-02-10 00:37:17 +0700

    feat: create, update, delete course

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -3,6 +3,9 @@ import Layout from '../components/Layout';
 import { Table, Button, Form, InputGroup } from 'react-bootstrap';
 import { Search, Plus } from 'lucide-react';
 import { api } from '../services/api';
+import CreateModal from '../components/courses/CreateModal';
+import logo from '../assets/images/logo.png';
+import EditModal from '../components/courses/EditModal';
 
 interface Course {
   id: number;
@@ -15,7 +18,10 @@ interface Course {
 export default function Courses() {
   const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
   const [courses, setCourses] = useState<Course[]>([]);
+  const [course, setCourse] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
+  const [createModalShow, setCreateModalShow] = useState(false);
+  const [editModalShow, setEditModalShow] = useState(false);
 
   useEffect(() => {
     api.get('/courses/data-course').then(res => {
@@ -23,17 +29,25 @@ export default function Courses() {
     }).catch(error => {
       console.error(error);
     });
-  }, []);
+  }, [createModalShow, editModalShow]);
 
   const filteredCourses = courses.filter((course) => {
     return course.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
+  const handleEditCourse = (e: any) => {
+    api.get(`/courses/data-course/${e}`).then(res => {
+      setCourse(res);
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
   return (
     <Layout>
       <div className="d-flex justify-content-between align-items-center mb-4">
         <h2>Data Course</h2>
-        <Button variant="primary" className="d-flex align-items-center gap-2 rounded-pill px-3">
+        <Button variant="primary" className="d-flex align-items-center gap-2 rounded-pill px-3" onClick={() => setCreateModalShow(true)}>
           Tambah Course
           <Plus size={20} />
         </Button>
@@ -71,13 +85,20 @@ export default function Courses() {
             <tr key={course.id}>
               <td>{index + 1}</td>
               <td>
-                <img src={`${BASE_URL}/${course.picture}`} alt={course.name} width={50} height={50}/>
+                <img src={course.picture !== null ? `${BASE_URL}/${course.picture}` : logo} alt={course.name} width={50} height={50}/>
               </td>
               <td>{course.name}</td>
               <td>{course.TrainingFocu.name}</td>
               <td>{course.numberOfPractices}</td>
               <td>
-                <Button variant="link" className="p-0">
+                <Button 
+                  variant="link" 
+                  className="p-0"
+                  onClick={() => {
+                    setEditModalShow(true);
+                    handleEditCourse(course.id);
+                  }}
+                >
                   Edit
                 </Button>
               </td>
@@ -85,6 +106,17 @@ export default function Courses() {
           ))}
         </tbody>
       </Table>
+
+      <CreateModal
+        show={createModalShow}
+        onHide={() => setCreateModalShow(false)}
+      />
+
+      <EditModal
+        show={editModalShow}
+        onHide={() => setEditModalShow(false)}
+        course={course}
+      />
     </Layout>
   );
 }
