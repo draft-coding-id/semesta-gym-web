089ae6a4840e93bc98f16e64765d08b519a62d0c
nafiswatsiq <amrullohnafis@gmail.com>
2025-04-09 13:57:34 +0700

    feat: add top trainer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Carousel, Col, Container, Row } from "react-bootstrap";
+import { Badge, Button, Card, Carousel, Col, Container, Row } from "react-bootstrap";
 import HeroImg from '../assets/images/hero-img.png';
 import BaseLayout from "../components/BaseLayout";
 import App1 from '../assets/images/app-1.png';
@@ -7,14 +7,40 @@ import App3 from '../assets/images/app-3.png';
 import Dev1 from '../assets/images/dev-1.png';
 import Dev2 from '../assets/images/dev-2.png';
 import Dev3 from '../assets/images/dev-3.png';
-import Slider11 from '../assets/images/slider-1-1.jpg';
-import Slider12 from '../assets/images/slider-1-2.jpg';
-import Slider13 from '../assets/images/slider-1-3.jpg';
 import Slider21 from '../assets/images/slider-2-1.jpg';
 import Slider22 from '../assets/images/slider-2-2.jpg';
 import Slider23 from '../assets/images/slider-2-3.png';
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import logo from '../assets/images/logo.png'; 
+import { Star } from "lucide-react";
+
+interface Trainer {
+  id: number;
+  picture: string;
+  User: any;
+  TrainingFocus: any;
+  description: string;
+  phone: string;
+  hoursOfPractice: string;
+  price: string;
+  rating: number;
+}
 
 export default function Home() {
+  const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL
+  const [trainers, setTrainers] = useState<Trainer[]>([]);
+
+  useEffect(() => {
+    api.get('/trainers/top-trainers').then(res => {
+      setTrainers(res)
+    }).catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+  console.log(trainers);
+
   return (
     <BaseLayout>
       <Container fluid >
@@ -94,41 +120,35 @@ export default function Home() {
       </Container>
       <Container fluid className="bg-primary-custom text-light py-5 bg-black">
         <h1 className="text-center">
-          GYM For Your Own
+          TOP PERSONAL TRAINER
         </h1>
       </Container>
-      <Container fluid className="py-5">
-        <Carousel>
-          <Carousel.Item>
-            <div className="position-relative" style={{ height: '600px', overflow: 'hidden', alignItems: 'center', objectFit: 'cover', display: 'flex' }}>
-              <img src={Slider11} alt="Hero" className="img-fluid" width="100%" height="100%"/>
-            </div>
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className="position-relative" style={{ height: '600px', overflow: 'hidden', alignItems: 'center', objectFit: 'cover', display: 'flex' }}>
-              <img src={Slider12} alt="Hero" className="img-fluid" width="100%" height="100%"/>
-            </div>
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className="position-relative" style={{ height: '600px', overflow: 'hidden', alignItems: 'center', objectFit: 'cover', display: 'flex' }}>
-              <img src={Slider13} alt="Hero" className="img-fluid" width="100%" height="100%"/>
-            </div>
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <Container fluid className="py-5 px-5">
+        <Row className="justify-content-center mx-5">
+          {trainers.map((trainer, index) => ( 
+            <Col md={4} key={index}>
+              <Card>
+                <Card.Img variant="top" src={trainer.picture !== null ? BASE_URL + '/' + trainer.picture : logo} />
+                <Card.Body>
+                  <Card.Title>
+                    <Row>
+                      <Col md={9}>
+                        {trainer.User.name}
+                      </Col>
+                      <Col md={3} className="text-end">
+                        <Badge pill bg="dark">
+                          <Star size={20} className="text-warning me-1" />
+                          {trainer.rating}
+                        </Badge>
+                      </Col>
+                    </Row>
+                  </Card.Title>
+                  <Card.Text>{trainer.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
       <Container fluid className="bg-primary-custom text-light py-5 bg-black">
         <h1 className="text-center">
